3c5bbdedd1861882787b424b15761d2aa180c6f8
Abhishek Joshi <56931504+awesome-aj0123@users.noreply.github.com>
2020-10-24 09:07:39 -0500

    Update manage_my_day.py

--- manage_my_day.py
+++ manage_my_day.py
@@ -41,7 +41,7 @@ class ScheduleGenerator():
         consistency_score = round(self.calculate_consistency_score(schedule), 3)
         time_period_score = self.calculate_time_period_score(schedule)
 
-        fin_score = 0.8*priority_score + 1*break_score + 0.5*consistency_score + 0.8*time_period_score
+        fin_score = 0.8*priority_score + 0.5*break_score + 0.8*consistency_score + 0.8*time_period_score
 
         # FOR TESTING PURPOSES
         # if(fin_score > 20):
@@ -90,6 +90,9 @@ class ScheduleGenerator():
                 diff = i - previous_break_index
                 score += -(0.5*diff - 1.5)**2 + 4
                 previous_break_index = i
+
+        diff = i - previous_break_index
+        score += -(0.5*diff - 1.5)**2 + 4
 
         return score
 
@@ -166,52 +169,249 @@ def crossover(population, time_slots):
     return new_population
 
 
+class DayManager():
+
+    def genetic_algo(self, event_tuples, start_time, end_time):
+
+        event_list = self.gather_dynamic_events(event_tuples)
+
+        # # putting the events and priorities into a dictionary
+        # # the event ('break', 1) will automatically be added to the dictionary
+        # event_list = {'work': 3, 
+        #               'running': 1,  
+        #               'tennis': 2, 
+        #               'volunteer': 2,
+        #               'break': 1}
+
+        total_time_slots = self.calculate_slots(event_tuples, start_time, end_time)
+        time_slots       = self.substract_fixed(event_tuples, total_time_slots)
+
+        env = ScheduleGenerator(event_list, time_slots)
+
+        num_generations = 100
+        num_population  = 300
+
+        avg_score = 0
+
+        original_population = env.randomly_generate_schedules(num_population)
+
+        best_schedule = []
+        best_score = 0
+        
+        for gen in range(num_generations):
+
+            original_population_with_scores = []
+
+            # randomly generates population of schedules for the very beginning
+
+            for schedule in original_population:
+                
+                # calculate the fitness for each of the schedules
+                score = env.fitness_function(schedule)
+                avg_score += score
+
+                original_population_with_scores.append((schedule, score))
+
+                if score > best_score:
+                    best_schedule = schedule
+                    best_score    = score 
+
+            avg_score /= len(original_population)
+            avg_score = round(avg_score, 3)
+
+            good_schedules = discard_bad_schedules(original_population_with_scores, avg_score)
+
+            original_population = crossover(original_population_with_scores, time_slots)
+
+        #print(f'Best schedule: {best_schedule}')
+        #print(f'Best score: {best_score}')
+    
+        final_schedule = self.generate_final_schedule(total_time_slots, time_slots, event_tuples, best_schedule, start_time, end_time)
+
+        return final_schedule
+        
+
+    def generate_final_schedule(self, total_time_slots, time_slots, event_tuples, best_schedule, start_time, end_time):
+        # start_time and end_time are strings
+
+        # get_slot_number
+
+        final_schedule = ['' for i in range(48)]
+
+        for event in event_tuples:
+            if event[4] == True: # fixed event
+                starting_slot = self.get_slot_number(event[1])
+                ending_slot   = self.get_slot_number(event[2])
+                for i in range(starting_slot, ending_slot + 1):
+                    final_schedule[i] = event[0]
+
+        start_slot_dynamic_events = self.get_slot_number(start_time)
+        
+        i = 0
+        index = start_slot_dynamic_events
+        while i < len(best_schedule):
+            event = best_schedule[i]
+
+            if final_schedule[index] == '':
+                final_schedule[index] = (event, index)
+                i+=1
+            index+=1
+
+        final_schedule = [x for x in final_schedule if x != '']
+
+        final_schedule_with_times = [(x[0], self.get_time_from_slot(x[1])) for x in final_schedule if type(x) != str]
+
+        final = []
+
+        curr_event = final_schedule_with_times[0][0]
+        start_time = final_schedule_with_times[0][1]
+        curr_time  = final_schedule_with_times[0][1]
+
+        for i in range(1, len(final_schedule_with_times)):
+
+            event = final_schedule_with_times[i]
+
+            if event[0] != curr_event or self.get_slot_number(curr_time)+1!=self.get_slot_number(event[1]):
+
+                final.append((curr_event, self.get_time_from_slot(self.get_slot_number(start_time)+1), self.get_time_from_slot(self.get_slot_number(curr_time)+2)))
+                start_time = event[1]
+
+            curr_time  = event[1]
+            curr_event = event[0]
+
+        final.append((curr_event, self.get_time_from_slot(self.get_slot_number(start_time)+1), self.get_time_from_slot(self.get_slot_number(curr_time)+2)))
+
+        return final
+
+    def calculate_slots(self, event_tuples, start_time, end_time):
+        # calcualates the number of slots taken given start and end times
+
+        self.event_times = []
+
+        curr_time = self.round_up_time(start_time)
+
+        # first, turn the times into numbers
+        hour1    = int(start_time[:2])
+        minutes1 = int(start_time[3:])
+        hour2    = int(end_time[:2])
+        minutes2 = int(end_time[3:]) 
+        
+        slot_min1 = 0
+        if minutes1 > 0 and minutes1 <= 30:
+            slot_min1+=1
+        elif minutes1 > 30:
+            slot_min1+=2
+
+        slot_min2 = 0
+        if minutes2 >= 30:
+            slot_min2+=1
+
+        slot_start = 2*hour1 + slot_min1
+        slot_end   = 2*hour2 + slot_min2
+
+        total = (slot_end - slot_start)
+
+        return total
+
+    def substract_fixed(self, event_tuples, total):
+        # substract the times from the fixed events
+        for event in event_tuples:
+            if event[4] == True:
+                # subtract the slots
+                total -= self.calculate_event_slots(event[1], event[2])
+
+        # then calculate and return the difference between the numbers
+        return total
+
+    def round_up_time(self, start_time):
+        hour    = int(start_time[:2])
+        minutes = int(start_time[3:])
+        if minutes > 30:
+            hour += 1
+            minutes = 0
+
+        return "{0:0=2d}".format(hour) + ':' + "{0:0=2d}".format(minutes)
+ 
+    def calculate_event_slots(self, start_time, end_time):
+        hour1    = int(start_time[:2])
+        minutes1 = int(start_time[3:])
+        hour2    = int(end_time[:2])
+        minutes2 = int(end_time[3:]) 
+
+        slot_min1 = 0
+        if minutes1 >= 30:
+            slot_min1+=1
+
+        slot_min2 = 0
+        if minutes2 > 0 and minutes2 <= 30:
+            slot_min2+=1
+        elif minutes2 > 30:
+            slot_min2+=2
+
+        slot_start = 2*hour1 + slot_min1
+        slot_end   = 2*hour2 + slot_min2
+
+        return slot_end - slot_start
+
+
+    def gather_dynamic_events(self, event_tuples):
+
+        dynamic_events = {}
+
+        for event_des in event_tuples:
+            
+            if not event_des[len(event_des) - 1]:
+                dynamic_events[event_des[0]] = event_des[3]
+
+        dynamic_events['break'] = 1
+
+        return dynamic_events
+
+    def get_slot_number(self, time):
+        hour    = int(time[:2])
+        minutes = int(time[3:])
+
+        slot_min = 0
+        if minutes > 0 and minutes <= 30:
+            slot_min+=1
+        elif minutes > 30:
+            slot_min+=2
+
+        #print(hour*2 + slot_min)
+        return hour*2 + slot_min - 1
+
+    def get_time_from_slot(self, slot_number):
+        hour   = math.floor(slot_number / 2)
+        minute = 30
+
+        if slot_number % 2 == 0:
+            minute = 0
+
+        #print("{0:0=2d}".format(hour) + ':' + "{0:0=2d}".format(minute))
+        return "{0:0=2d}".format(hour) + ':' + "{0:0=2d}".format(minute)
+
+
+def get_schedule(event_tuples = [('math class', '09:00', '10:35', 4, True),
+                                 ('english class', '14:00', '15:30', 4, True),
+                                 ('study english', '', '', 3, False),
+                                 ('study com sci', '', '', 3, False),
+                                 ('running', '', '', 1, False),
+                                 ('tennis', '', '', 2, False),
+                                 ('volunteer', '', '', 2, False)],
+                start_time    = '07:47',
+                end_time      = '20:40'):
+
+    manager = DayManager()
+
+    final_schedule = manager.genetic_algo(event_tuples, start_time, end_time)
+
+    for event in event_tuples:
+        if event[4] == True:
+            final_schedule.append((event[0], event[1], event[2]))
+
+    return final_schedule
+
 if __name__ == '__main__':
 
-    # putting the events and priorities into a dictionary
-    # the event ('break', 1) will automatically be added to the dictionary
-    event_list = {'work': 5, 
-                  'running': 2,  
-                  'tennis': 3, 
-                  'volunteer': 3,
-                  'break': 1}
-
-    time_slots = 20
-    env = ScheduleGenerator(event_list, time_slots)
-
-    num_generations = 100
-    num_population  = 300
-
-    avg_score = 0
-
-    original_population = env.randomly_generate_schedules(num_population)
-
-    generation_averages = []
-    
-    for gen in range(num_generations):
-
-        original_population_with_scores = []
-
-        # randomly generates population of schedules for the very beginning
-
-        for schedule in original_population:
-            
-            # calculate the fitness for each of the schedules
-            score = env.fitness_function(schedule)
-            avg_score += score
-
-            original_population_with_scores.append((schedule, score))
-
-        avg_score /= len(original_population)
-        avg_score = round(avg_score, 3)
-        generation_averages.append(avg_score)
-
-        print(original_population[0])
-        print(f'Average Score => {avg_score}')
-
-        good_schedules = discard_bad_schedules(original_population_with_scores, avg_score)
-
-        original_population = crossover(original_population_with_scores, time_slots)
-
-    plt.plot(generation_averages)
-    plt.show()
+    final_schedule = get_schedule()
+    print(final_schedule)
